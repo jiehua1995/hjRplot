57d2dedc297f2c0d617252265eb89608385b440a
Hua, Jie <Jie.Hua@lmu.de>
2025-01-16 17:49:13 +0100

    up

--- demo_paf.R
+++ demo_paf.R
@@ -3,9 +3,10 @@ library(hjRplot)
 ali <- read.paf(choose.files())
 # Filter the alignment with mapping quality > 30
 
-ali_selected <- subset(ali, tname == "chr3R_mel")
-ali_selected <- subset(ali_selected, qname == "chr3R_sim")
-ali_selected <- subset(ali_selected, mapq >= 60)
+
+ali_selected <- subset(ali, tname == "chrX_mel")
+ali_selected <- ali
+ali_selected <- subset(ali_selected, mapq >= 30)
 ali_selected <- subset(ali_selected, tp == "P")
 #ali_selected$percentage <- ali_selected$nmatch / ali_selected$alen
 #selected_reads <- c("39fec261-134e-4b8a-b1f2-adad54f8f7ab","c8a109ee-ab3f-4584-abad-76cb30f050e4","00feb67d-a366-4cc7-babf-16cd19871e8d","c8a109ee-ab3f-4584-abad-76cb30f050e4;00352179-85eb-459a-9d25-b56756a10f75","87195fb4-04ef-4ac2-9bcd-06ae724aaf64")
@@ -17,7 +18,7 @@ ali_selected_filter <- subset(ali_selected, percentage >= 0.8)
 
 
 ali_selected <- subset(ali_selected, tend >= 27920-1000 & tstart <= 118193+1000)
-paf_selected_coverage(ali_selected, chr="chr3R_mel", mapping_type="P")
+paf_selected_coverage(ali_selected, chr="chrX_mel", mapping_type="P")
 
 # Genomic fasta NNN finding
 fasta <- "C:\\Users\\ra35yun\\Downloads\\temp\\temp\\Ras3_renamed_sorted.fasta"
